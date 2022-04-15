690427e82caa7584cec1459bd9fa2d0adea24fdf
Josh Warren <josh@thepinballbay.com>
2022-04-15 12:02:08 -0700

    Initial Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (lat, lon, callback) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=93f438df3fe9179ad9cdef13100cedea&query=' + lat + ',' + lon + '&units=f'
+    console.log('Location', lat, lon)
+    request({url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to access weather service.', undefined)
+        } else if (body.error) {
+            callback('Unable to find location.', undefined)
+        } else {
+            callback(undefined, 'The current temperature is ' + body.current.temperature + ' degrees.  It feels like ' + body.current.feelslike + ' degrees.')
+        }
+    })
+}
+
+module.exports = forecast
